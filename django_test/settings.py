4514a97f4952a6b49c1a9a39565a2263368511bb
Deanna <deanna.robertazzi@gmail.com>
2015-02-19 10:21:35 -0500

    Modified django_test/settings.py

--- django_test/settings.py
+++ django_test/settings.py
@@ -139,9 +139,9 @@ except Exception as e:
     print e.message 
     
 if not DEBUG:
-    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_STORAGE_BUCKET_NAME = os.environ['article-deanna']
+    AWS_ACCESS_KEY_ID = os.environ['AKIAITOMYLOJLV6SGC6A']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-    S3_URL = 'http://%s.s3.amazonaws.com/assets/' % AWS_STORAGE_BUCKET_NAME
+    S3_URL = 'http://%s.s3.amazonaws.com/assets/' % article-deanna 
     STATIC_URL = S3_URL
