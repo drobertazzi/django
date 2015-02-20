e511c549dd8e7cf43ad3b76166a49d5591e1ea81
Deanna <deanna.robertazzi@gmail.com>
2015-02-20 13:27:12 -0500

    Added missing comma

--- django_test/settings.py
+++ django_test/settings.py
@@ -54,7 +54,7 @@ LOGGING = {
     'handlers': {
         'mail_admins':{
             'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
+            'class': 'django.utils.log.AdminEmailHandler',
         }
     },
     'loggers': {
